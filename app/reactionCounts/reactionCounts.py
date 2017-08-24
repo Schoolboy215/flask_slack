9f6e6cbad74f00d110561d5558b546580365070f
James McKay <james.mckay215@yahoo.com>
2017-08-24 13:37:21 -0400

    Made the sorting work on dictionary values

--- app/reactionCounts/reactionCounts.py
+++ app/reactionCounts/reactionCounts.py
@@ -15,7 +15,7 @@ class slackResponse:
 class reactionCounts(Resource):
 	def get(self):
 		counts = pickle.load(open("reactions.p","rb"))
-		sorted_counts = sorted(counts.items(), key=operator.itemgetter(0), reverse=True)
+		sorted_counts = sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
 		response = slackResponse()
 		response.text = "Here are the current reaction counts:\n"
 		for reaction in sorted_counts:
